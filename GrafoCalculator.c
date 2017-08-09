c0c161d1eda1482d8d500e931c9a76ad6c8ba922
Martin Diaz <MDiaz@proveedor.personal.com.ar>
2017-08-09 18:04:42 -0300

    segundo comit con el menu mas completo opcion 2

--- GrafoCalculator.c
+++ GrafoCalculator.c
@@ -14,27 +14,19 @@ Ejercicio no12
 Desarrollar un algoritmo y luego codificarlo en C que permite consultar si entre dos
 vértices existe una arista de un grafo utilizando la matriz de incidencia.
 
-Ejercicio no13
-Desarrollar un algoritmo y luego codificarlo en C que dado un grafo dirigido A calcule
-la transpuesta de la matriz de adyacencia de A. ¿Qué información nos brinda la matriz
-transpuesta?
-
-Ejercicio no14
-Desarrollar un algoritmo y luego codificarlo en C que dada la matriz de incidencia de
-un grafo A, calcule A.At
-
-.¿Que representan los elementos de esa matriz producto?
 */
 
 int mostrarMenu();
 int menu();
 int crearMatrizAdyacencia(int matrizAdyacencia[10][10],int vertices);
 void crearMatrizIncidencia(int matrizAdyacencia[10][10],int vertices,int aristas);
-void dibujarMatrizAdyacencia(int matrizAdyacencia[10][10],int vertices);
+void dibujarMatriz(int matrizAdyacencia[10][10],int vertices, int aristas);
 void generarMatriz(int matrizAdyacencia[10][10]);
+void dibujarCabecera(int aristas);
 void mostrarGradoDeVertice(int matrizAdyacencia[10][10],int vertices);
 void calcularPorAdyacencia(int matrizAdyacencia[10][10],int vertices);
 void calcularPorIncidencia(int matrizAdyacencia[10][10],int vertices,int aristas);
+void consultarVertice(int matriz[10][10],int vertices);
 
 int main()
 {
@@ -56,14 +48,14 @@ int main()
             case 1 :
                 while(vertices < 1 || vertices > 10 )
                 {
-                    printf("Ingrese un valor entre 1 y 10 los vertices: " );
-                    scanf("%i", &vertices);
+                    printf("  Ingrese un valor entre 1 y 10 los vertices: " );
+                    scanf("  %i", &vertices);
                 };
 
                 aristas = crearMatrizAdyacencia(matrizAdyacencia,vertices);
                 printf(" \n" );
-                printf( "%i vertices elegidos, creando matriz ingrese los grados\n", vertices);
-                dibujarMatrizAdyacencia(matrizAdyacencia,vertices);
+                printf("  %i vertices elegidos, creando matriz ingrese los grados\n", vertices);
+                dibujarMatriz(matrizAdyacencia,vertices,vertices);
                 printf("  Numero de vertices: %d \n", vertices);
                 printf("  Numero de aristas: %d \n", aristas);
                 printf("  Valencia total: %d \n", aristas*2);
@@ -74,35 +66,34 @@ int main()
             case 2 :
                 while(vertices < 1 || vertices > 10 )
                 {
-                    printf("Ingrese un valor entre 1 y 10 los vertices: " );
-                    scanf("%i", &vertices);
+                    printf(  "  Ingrese un valor entre 1 y 10 para la cantidad de vertices: " );
+                    scanf("  %i", &vertices);
                 };
                 while(aristas < 0)
                 {
-                    printf("ingrese un entero mayor o igual a 0 para las aristas: " );
-                    scanf("%d", &aristas);
+                    printf("  Ingrese un entero mayor o igual a 0 para la cantidad de aristas: " );
+                    scanf("  %d", &aristas);
                 };
-                crearMatrizIncidencia(matrizAdyacencia,vertices,vertices);
+                crearMatrizIncidencia(matrizIncidencia,vertices,aristas);
                 break;
 
             case 3 :
-                while(vertices < 1 || vertices > 10 )
-                {
-                    printf("Ingrese un valor entre 1 y 10 los vertices: " );
-                    scanf("%i", &vertices);
+                while(vertices < 1 || vertices > 10 )                {
+                    printf("  Ingrese un valor entre 1 y 10 los vertices: " );
+                    scanf("  %i", &vertices);
                 };
                 break;
 
             case 4 :
                 printf(" \n" );
-                printf("Generando grafo con valores aleatorios \n" );
+                printf("  Generando grafo con valores aleatorios \n" );
                 generarMatriz(matrizAdyacencia);
                 printf(" \n" );
                 break;
 
             case 5 :
                 printf(" \n" );
-                printf("Saliendo del menu \n" );
+                printf("  Saliendo del menu \n" );
                 printf(" \n" );
                 break;
             default:
@@ -122,11 +113,11 @@ int crearMatrizAdyacencia(int matrizAdyacencia[10][10], int vertice)
 	{
 		for(j = i; j < vertice; j++)
 		{
-			printf("ingrese el numero de caminos entre el vertice %d y el vertice %d\n", i,j );
+			printf("  Ingrese el numero de caminos entre el vertice %d y el vertice %d\n", i,j );
 			matrizAdyacencia[i][j] = -1;
 			while(matrizAdyacencia[i][j] < 0)
 			{
-				printf("ingrese un entero positivo: " );
+				printf("  Ingrese un entero positivo: " );
 				scanf("%i", &matrizAdyacencia[i][j]);
 				matrizAdyacencia[j][i] = matrizAdyacencia[i][j];
 				aristas = aristas + matrizAdyacencia[i][j];
@@ -136,20 +127,22 @@ int crearMatrizAdyacencia(int matrizAdyacencia[10][10], int vertice)
     return aristas;
 }
 
-void dibujarMatrizAdyacencia(int matriz[10][10], int vertices)
+void dibujarMatriz(int matriz[10][10], int vertices, int aristas)
 {
 	int i, j;
-	printf("Dibujando la matriz: \n");
-    printf("\n");
+	printf("  Dibujando la matriz: \n");
+    dibujarCabecera(aristas);
+
 	for(i = 0; i < vertices ;i++)
 	{
-        printf("  ");
-		for(j = 0; j < vertices; j++)
+        printf(" %i |", i);
+		for(j = 0; j < aristas; j++)
 		{
-            printf(" %i", matriz[i][j]);
+            printf(" %i |", matriz[i][j]);
 		}
         printf("\n");
-        printf("\n");
+        printf("  ");
+        printf(" %.*s\n", (aristas*4+1), "==================================================================================");
 	}
     printf("\n");
 }
@@ -169,7 +162,6 @@ int menu()
             printf("  Seleccione una opcion [1 - 5]:      ");
             scanf("%i",&opcion);
 		}
-
 		return opcion;
 
 }
@@ -179,7 +171,54 @@ void calcularPorAdyacencia(int matrizAdyacencia[10][10],int vertices)
 
 };
 void calcularPorIncidencia(int matrizAdyacencia[10][10],int vertices,int aristas){};
-void crearMatrizIncidencia(int matrizAdyacencia[10][10],int vertices,int aristas){};
+void crearMatrizIncidencia(int matrizIncidencia[10][10],int vertices,int aristas)
+{
+    srand(time(NULL));
+    int i, j, n;
+    int conexion[vertices];
+    int sum = 0;
+
+	for(i = 0; i < vertices; i++)
+	{
+		for(j = 0; j < aristas; j++)
+		{
+		    matrizIncidencia[i][j] = 0;
+		    if(i == 0){
+                printf("\n");
+                matrizIncidencia[i][j]=(rand() % 2);
+		    }else if(i == vertices-1){
+                sum = 0;
+                for(n = i-1; n >= 0; n--){
+                    sum += matrizIncidencia[n][j];
+                }
+		        if(sum == 1){
+                    matrizIncidencia[i][j] = 1;
+                }else {
+                    matrizIncidencia[i][j] = 0;
+
+                }
+
+		    }else{
+                matrizIncidencia[i][j]= (rand() % 2);
+                if (matrizIncidencia[i][j] == 1){
+                    sum = 0;
+                    for(n = i-1; n >= 0; n--){
+                        sum += matrizIncidencia[n][j];
+                    }
+                    if(sum == 2){
+                        matrizIncidencia[i][j]=0;
+                        printf("%d\n", matrizIncidencia[i][j]);
+                    }
+                }
+
+		    }
+
+		}
+
+	}
+    dibujarMatriz(matrizIncidencia, vertices, aristas);
+    consultarVertice(matrizIncidencia, vertices);
+};
 void generarMatriz(int matrizAdyacencia[10][10])
 {
     srand(time(NULL));
@@ -190,14 +229,14 @@ void generarMatriz(int matrizAdyacencia[10][10])
 	{
 		for(j = i; j < vertices; j++)
 		{
-		    matrizAdyacencia[i][j] = (rand() % (3 - 0)) + 0;
-			printf("generado el numero de caminos entre el vertice %d y el vertice %d: %d\n", i,j,matrizAdyacencia[i][j] );
+		    matrizAdyacencia[i][j] = (rand() % 4);
+			printf("  Generado el numero de caminos entre el vertice %d y el vertice %d: %d\n", i,j,matrizAdyacencia[i][j] );
             matrizAdyacencia[j][i] = matrizAdyacencia[i][j];
             aristas = aristas + matrizAdyacencia[i][j];
 
 		}
 	}
-    dibujarMatrizAdyacencia(matrizAdyacencia, vertices);
+    dibujarMatriz(matrizAdyacencia, vertices, vertices);
     printf("  Numero de vertices: %d \n", vertices);
     printf("  Numero de aristas: %d \n", aristas);
     printf("  Valencia total: %d \n", aristas*2);
@@ -213,11 +252,15 @@ void mostrarGradoDeVertice(int matrizAdyacencia[10][10],int vertices)
 
     for(i; i < vertices; i++)
 	{
-		for(j = i; j < vertices; j++)
+		for(j = 0; j < vertices; j++)
 		{
-          sum += matrizAdyacencia[i][j];
-		}
+        if( i == j){
+            sum += matrizAdyacencia[i][j]*2;
+        }else{
+             sum += matrizAdyacencia[i][j];
+        }
 
+		}
 		printf("  La valencia del vertice %d: %d \n", i+1,sum);
 		if(sum > max){
             vertMax = i + 1;
@@ -228,3 +271,41 @@ void mostrarGradoDeVertice(int matrizAdyacencia[10][10],int vertices)
     printf("  El vertice con mayor grado es %d con %d \n", vertMax,max);
 }
 
+void dibujarCabecera(int aristas)
+{
+    int i = 0;
+    printf(" \\  ");
+    for(i; i < aristas; i++)
+	{
+	    printf(" %i  ", i);
+	}
+     printf("\n %.*s==\n", (aristas*4+1), "==================================================================================");
+}
+
+void consultarVertice(int matriz[10][10], int vertices)
+{
+
+    int vert1 = -1;
+    int vert2 = -1;
+    char opcion = 'z';
+    printf("  Desea consultar si existe una arista entre dos vertices S/N: \n");
+
+    while(opcion != 'S' && opcion != 'N' && opcion != 's' && opcion != 'n')
+    {
+        scanf("%c",&opcion);
+    }
+    fflush(stdin);
+    if(opcion == 'S' || opcion == 's')
+    {
+        printf("  Ingrese el primer vertice (desde 0 hasta %d): \n", vertices-1);
+        while(vert1 < 0 || vert1 > vertices-1 ){
+            scanf("%i",&vert1);
+        }
+
+        printf("  Ingrese el segundo (desde 0 hasta %d): \n", vertices-1);
+        while(vert2 < 0 || vert2 > vertices-1 ){
+            scanf("%i",&vert2);
+        }
+    }
+
+};
--- test.c
+++ test.c
@@ -9,16 +9,24 @@ int main()
     int div = 0;
     //scanf("%i", &div);
     srand(time(NULL));
-    int r = (rand() % (10 - 1)) + 1;
-    printf("tu numero 1 es %d\n", (rand() % (3 - 0)) + 0);
-    printf("tu numero 2 es %d\n", (rand() % (3 - 0)) + 0);
-    printf("tu numero 3 es %d\n", (rand() % (3 - 0)) + 0);
-    printf("tu numero 4 es %d\n", (rand() % (3 - 0)) + 0);
+    int r = (rand() % 10);
+    int pr = 1;
+    printf("%d\n",(r*4+1));
+    printf(" %.*s\n", (r*4+1), "==================================================================================");
+
+    /*printf("tu numero 1 es %d\n", (rand() % 4));
+    printf("tu numero 2 es %d\n", (rand() % 4));
+    printf("tu numero 3 es %d\n", (rand() % 4));
+    printf("tu numero 4 es %d\n", (rand() % 4));
+    printf("tu numero 1 es %d\n", (rand() % 4));
+    printf("tu numero 2 es %d\n", (rand() % 4));
+    printf("tu numero 3 es %d\n", (rand() % 4));
+    printf("tu numero 4 es %d\n", (rand() % 4));
 
     if((div%2)==0 ){
         printf("el numero es par la division es %d", div/2);
     }else{
         printf("el numero es inpar la division es %d", div/2+1);
-    }
+    }*/
 
 }
